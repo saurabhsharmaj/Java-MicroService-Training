2ab141e3658b79a409ee7ca45b40abe2afbe01f5
DESKTOP-0AQCN09\win 11 <ersaurabhsharma.mca@gmail.com>
2025-05-02 19:50:08 +0530

    update the promtail

--- k8s/k8s.bat
+++ k8s/k8s.bat
@@ -2,4 +2,6 @@ kubectl delete -f deployment.yaml
 kubectl delete -f service.yaml
 kubectl apply -f deployment.yaml
 kubectl apply -f service.yaml
+kubectl create configmap promtail-config --from-file=promtail-config.yml
+kubectl apply -f promtail-deployment.yaml
 kubectl get all
