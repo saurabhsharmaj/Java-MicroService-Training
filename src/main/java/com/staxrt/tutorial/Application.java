f6c147536c0b269b5001f9102c27cfd4895ea347
DESKTOP-0AQCN09\win 11 <ersaurabhsharma.mca@gmail.com>
2025-05-13 20:11:28 +0530

    update micrometer

--- src/main/java/com/staxrt/tutorial/Application.java
+++ src/main/java/com/staxrt/tutorial/Application.java
@@ -1,7 +1,12 @@
 package com.staxrt.tutorial;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import io.micrometer.core.instrument.MeterRegistry;
 
 /**
  * The type Application.
@@ -19,4 +24,10 @@ public class Application {
   public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+  
+  @Bean
+  MeterRegistryCustomizer<MeterRegistry> configurer(
+      @Value("${spring.application.name}") String applicationName) {
+      return (registry) -> registry.config().commonTags("application", applicationName);
+  }
 }
